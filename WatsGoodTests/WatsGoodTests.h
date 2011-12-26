8d59d4b023bacdbb73d454b1a5c27a140f091904
Tom <Tom@toms-mac-pro.home>
2011-12-26 05:47:50 -0500

    Initial Commit

---
+++ WatsGoodTests/WatsGoodTests.h
@@ -0,0 +1,13 @@
+//
+//  WatsGoodTests.h
+//  WatsGoodTests
+//
+//  Created by Tom on 12/26/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WatsGoodTests : SenTestCase
+
+@end
---
+++ WatsGoodTests/WatsGoodTests.m
@@ -0,0 +1,32 @@
+//
+//  WatsGoodTests.m
+//  WatsGoodTests
+//
+//  Created by Tom on 12/26/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "WatsGoodTests.h"
+
+@implementation WatsGoodTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in WatsGoodTests");
+}
+
+@end
